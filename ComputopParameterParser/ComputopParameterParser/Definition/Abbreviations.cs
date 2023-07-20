3465b85defb648cab27096e666ce34b349b64fd5
ZhichaoXiang <z.xiang@journaway.com>
2023-07-20 18:41:41 +0000

    computop

--- ComputopParameterParser/ComputopParameterParser/Definition/Abbreviations.cs
+++ ComputopParameterParser/ComputopParameterParser/Definition/Abbreviations.cs
@@ -9,12 +9,14 @@ namespace ComputopParameterParser.Definition
     {
         private static readonly Dictionary<string, JsonSchemaInfo> _dict = new Dictionary<string, JsonSchemaInfo>()
         {
-            { "CND", new JsonSchemaInfo(){IsRequired=false } },
-            { "M", new JsonSchemaInfo(){IsRequired=true } },
-            { "O", new JsonSchemaInfo(){IsRequired=false } },
-            { "C", new JsonSchemaInfo(){IsRequired=false } },
-            { "OM", new JsonSchemaInfo(){IsRequired=false } },
+            {"CND", new JsonSchemaInfo() {IsRequired = false}},
+            {"M", new JsonSchemaInfo() {IsRequired = true}},
+            {"O", new JsonSchemaInfo() {IsRequired = false}},
+            {"C", new JsonSchemaInfo() {IsRequired = false}},
+            {"OM", new JsonSchemaInfo() {IsRequired = false}},
+            {"OC", new JsonSchemaInfo() {IsRequired = false}},
         };
+
         public static JsonSchemaInfo GetJsonSchemaInfo(DefinitioinInfo di)
         {
             return _dict[di.Condition];
