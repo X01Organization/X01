ca29b4874b2433fad6ca0dff271cad97c2cea1c1
ZhichaoXiang <z.xiang@journaway.com>
2025-05-21 15:26:37 +0000

    develop

--- vscode-extensions/command-runner/src/extension.ts
+++ vscode-extensions/command-runner/src/extension.ts
@@ -1,25 +1,35 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "dotnet-test-runner" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "command-runner" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "dotnet-test-runner.runTests",
+    async (contextSelection: vscode.Uri, allSelections: vscode.Uri[] = []) => {
+      const fsPath = contextSelection?.fsPath ?? "No file selected";
+      const selectedPaths =
+        allSelections.length > 0
+          ? allSelections.map((uri) => uri.fsPath)
+          : [contextSelection.fsPath];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('command-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from X01 Command Runner!');
-	});
+      vscode.window.showInformationMessage(
+        `Run tests on:\n${selectedPaths.join("\n") } \n ${fsPath}`
+      );
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
