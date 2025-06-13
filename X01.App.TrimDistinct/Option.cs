9471fbadcf17e0558e47ab88eef147738d11a36a
ZhichaoXiang <zhichaoxiang86@gmail.com>
2025-06-13 08:51:24 +0000

    develop: ??

---
+++ X01.App.TrimDistinct/Option.cs
@@ -0,0 +1,12 @@
+﻿using X01.CmdLine;
+
+namespace X01.App.TrimDistinct;
+
+public class Option
+{
+    [CmdLineArgs(ShortName = "i", LongName = "input", HelpText = "the input file")]
+    public string? Input { get; set; }
+
+    [CmdLineArgs(ShortName = "o", LongName = "output", HelpText = "the output file")]
+    public string? Output { get; set; }
+}
