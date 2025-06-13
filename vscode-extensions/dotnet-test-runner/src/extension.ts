f824c774b0ad7d984287de4c4c27a06b93b44bdc
ZhichaoXiang <zhichaoxiang86@gmail.com>
2025-06-13 08:15:39 +0000

    develop: ??

--- vscode-extensions/dotnet-test-runner/src/extension.ts
+++ vscode-extensions/dotnet-test-runner/src/extension.ts
@@ -1,36 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "dotnet-test-runner" is now active!'
-  );
+  const taskProvider = vscode.tasks.registerTaskProvider("X01", {
+    provideTasks: () => {
+      return []; // optional predefined tasks
+    },
+    resolveTask,
+  });
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "dotnet-test-runner.runTests",
-    async (contextSelection: vscode.Uri, allSelections: vscode.Uri[] = []) => {
-      const fsPath = contextSelection?.fsPath ?? "No file selected";
-      const selectedPaths =
-        allSelections.length > 0
-          ? allSelections.map((uri) => uri.fsPath)
-          : [contextSelection.fsPath];
-
-      vscode.window.showInformationMessage(
-        `Run tests on:\n${selectedPaths.join("\n") } \n ${fsPath}`
-      );
-    }
-  );
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(taskProvider);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function resolveTask(task: vscode.Task): vscode.Task | undefined {
+  const definition = task.definition as any;
+
+  const command = definition.command;
+  const args = definition.args || [];
+
+  if (!command) {
+    return undefined;
+  }
+
+  // Join command and args for full command string
+  const fullCommand = [command, ...args].join(" ");
+  console.log(`Resolved task command: ${fullCommand}`); // Logs to Developer Tools console
+  vscode.window.showInformationMessage(`Running: ${fullCommand}`); // Optional popup
+
+  const shellExec = new vscode.ShellExecution(command, args);
+  return new vscode.Task(
+    definition,
+    task.scope ?? vscode.TaskScope.Workspace,
+    task.name,
+    "X01",
+    shellExec,
+    task.problemMatchers
+  );
+}
